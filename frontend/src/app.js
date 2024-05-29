28ff3b5b660c405ab457ab2997e8d93c24a5765b
Niraj Kumar <149550225+NirajRoy43@users.noreply.github.com>
2024-05-29 08:58:46 +0530

    Create app.js

---
+++ frontend/src/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import CodeEditor from './components/CodeEditor';
+
+function App() {
+  return (
+    <div className="App">
+      <CodeEditor questionId={1} />
+    </div>
+  );
+}
+
+export default App;
